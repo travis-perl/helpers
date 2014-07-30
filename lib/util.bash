ab859d83f72d4b162f6be5f103c29a092d7e5105
Graham Knop <haarg@haarg.org>
2014-07-30 06:47:59 -0400

    timeouts for progress meters

--- lib/util.bash
+++ lib/util.bash
@@ -1,6 +1,8 @@
 function clean-up {
-  kill "$1" || true
-  wait "$1" 2>/dev/null || true
+  for pid; do
+    kill "$pid" >/dev/null 2>&1 || continue
+    wait "$pid" >/dev/null 2>&1 || true
+  done
 }
 
 function show-verbose {
@@ -8,25 +10,39 @@ function show-verbose {
 }
 
 function show-on-fail {
+  local log_file="$(mktemp -t log-XXXXXX)"
+  ( exec "$@" > "$log_file" 2>&1 < /dev/null ) &
+  local child="$!"
+  local start=$(date +%s)
+  local timeout="$TIMEOUT"
+  [ -z "$timeout"] && timeout=$(( 30*60 ))
+  local expire=$((start + timeout))
   (
-    while true; do
-      sleep 5
+    now="$start"
+    while (( now < expire )); do
+      sleep 1
       printf '.'
+      now=$(date +%s)
     done
+    kill "$child" >/dev/null 2>&1
   ) &
   local prog="$!"
-  trap "clean-up $prog; echo 'Aborted.'; exit 1" SIGHUP SIGINT SIGTERM
-  local log_file="$(mktemp -t log-XXXXXX)"
   local status=0
-  ( "$@" > "$log_file" 2>&1 ) || status="$?"
+  trap "clean-up $prog $child" SIGHUP SIGINT SIGTERM
+  wait "$child" || status="$?"
   clean-up "$prog"
   trap - SIGHUP SIGINT SIGTERM
-  if [ "$status" != 0 ]; then
+  if [ "$status" == 0 ]; then
+    echo ' Done.'
+  elif [ "$status" == "143" ]; then
+    echo " Timeout! $((expire - start))s"
+    cat "$log_file" 1>&2
+  elif (( status > 128 )); then
+    echo " Killed! (signal $((status - 128)))"
+  else
     echo ' Failed!'
     cat "$log_file" 1>&2
-  else
-    echo ' Done.'
   fi
-  rm "$log_file"
+  rm -f "$log_file"
   return "$status"
 }
